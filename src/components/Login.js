807909cb63666ada68e36e672bb66082d6adf99a
lightwheel10 <tiwariparas1096@gmail.com>
2024-08-08 02:32:15 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { login } from './api';
+// import { login } from './api';
 import { Link, useNavigate } from 'react-router-dom';
 import { Home } from 'lucide-react';
 
