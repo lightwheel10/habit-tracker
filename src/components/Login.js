45d6e2f305d413da69ac98b61f584bd9ed57c73c
lightwheel10 <tiwariparas1096@gmail.com>
2024-08-08 02:26:19 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = ({ setIsLoggedIn, setUserType }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://us-central1-habit-tracker-7df86.cloudfunctions.net/api', { email, password },
+      const response = await axios.post('https://us-central1-habit-tracker-7df86.cloudfunctions.net/api/auth/login', { email, password },
         {
         headers: { 'Content-Type': 'application/json' },
         withCredentials: true
